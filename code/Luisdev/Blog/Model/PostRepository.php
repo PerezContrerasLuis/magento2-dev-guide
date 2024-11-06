ef86a7eab3c07b3aff6d40531f25bf7362a30a51
PerezContrerasLuis <luisperezcontreras@gmail.com>
2024-11-06 17:31:34 -0600

    Usando PostRepository para leer datos del ResourceModel
    PostRepository es la clase que usamos para consultar datos de la base de datos a través del ResourceModel. Básicamente, este repositorio actúa como una capa intermedia que organiza la lógica de consulta y facilita la lectura de datos, sin necesidad de interactuar directamente con el ResourceModel en otras partes del código.
    
    1.	__construct:
    Este es el constructor de PostRepository. Aquí inyectamos las dependencias necesarias, PostFactory y PostResourceModel, que permiten crear y cargar publicaciones desde la base de datos. En pocas palabras, el constructor prepara los ingredientes que usaremos en los métodos de la clase.
    
    2.	getById:
    El método getById es el encargado de buscar una publicación en la base de datos usando su ID. Primero, se crea un objeto de Post usando postFactory, y luego postResourceModel->load() se encarga de cargar los datos de esa publicación. Si no encuentra la publicación (es decir, si el ID no existe), lanza una excepción NoSuchEntityException con un mensaje informativo. Si encuentra el Post, simplemente lo devuelve.

--- code/Luisdev/Blog/Model/PostRepository.php
+++ code/Luisdev/Blog/Model/PostRepository.php
@@ -4,14 +4,37 @@ namespace Luisdev\Blog\Model;
 
 use Luisdev\Blog\Api\Data\PostInterface;
 use Luisdev\Blog\Api\PostRepositoryInterface;
+use Luisdev\Blog\Model\ResourceModel\Post as PostResourceModel;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 
 class PostRepository implements PostRepositoryInterface
 {
+
+    /**
+     * @param PostFactory $postFactory
+     * @param PostResourceModel $resourceModelPost
+     */
+    public function __construct(
+        private PostFactory       $postFactory,
+        private PostResourceModel $postResourceModel
+    )
+    {
+    }
+
+
+    /**
+     * @param int $id
+     * @return PostInterface
+     */
     public function getById(int $id): PostInterface
     {
-        // TODO: Implement getById() method.
+        $post = $this->postFactory->create();
+        $this->postResourceModel->load($post, $id);
+        if (!$post->getId()) {
+            throw new NoSuchEntityException(__('No existe un post con el ID "%1" .', $id));
+        }
+        return $post;
     }
 
     public function save(PostInterface $post): PostInterface
