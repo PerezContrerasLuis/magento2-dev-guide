cb3c05520e29bb574ad235c95bb09b17f2b5e0e8
PerezContrerasLuis <luisperezcontreras@gmail.com>
2025-03-06 12:01:37 -0600

    Entendiendo las propiedades defaults
    Introducción
    
    Los UI Components en Magento pueden definir valores predeterminados mediante la propiedad defaults. Esto nos permite establecer configuraciones iniciales y personalizar el comportamiento del componente sin necesidad de redefinir cada valor manualmente. Estos valores pueden ser sobrescritos o modificados dinámicamente en la ejecución del código, brindando mayor flexibilidad y reutilización.
    
    Explicación del Código
    	1.	Propiedad defaults
    	•	Aquí se define un objeto donde se establecen valores predeterminados para el UI Component. En este caso, message contiene la cadena "Default Message ^_^".
    	•	Esta propiedad permite centralizar configuraciones iniciales y reutilizarlas dentro del componente sin necesidad de definirlas repetidamente.
    
    Este enfoque permite configurar valores iniciales de manera eficiente y acceder a ellos dentro de la lógica del componente, manteniendo el código más organizado y fácil de mantener.

--- code/Luisdev/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ code/Luisdev/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -2,9 +2,12 @@ define(['uiComponent'],function (component){
     'use strict';
     console.log('Basic Free shipping component UI is loaded');
     return component.extend({
+        defaults : {
+          message : 'Default Message' + '^_^'
+        },
         initialize : function () {
             this._super();
-            console.log('Extend uiComponet class');
+            console.log(this.message);
         }
     });
 });
