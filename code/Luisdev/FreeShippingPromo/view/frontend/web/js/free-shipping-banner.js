61ce7fcff5dc1b0d7f6d2c7ec0d81fe6e002d74b
PerezContrerasLuis <luisperezcontreras@gmail.com>
2025-03-07 14:02:10 -0600

    Vinculando propiedades con el DOM
    Se realizaron modificaciones para vincular dinámicamente los datos del UI Component con el DOM usando KnockoutJS.
    
    Cambios en la plantilla KnockoutJS
    	•	Se reemplazó texto estático por un span con data-bind="text: subtotal", permitiendo que el subtotal del carrito se muestre dinámicamente.
    
    Cambios en el UI Component
    	•	Se agregó la propiedad subtotal con un valor numérico.
    	•	Se creó la función formatCurrency() en el ViewModel para formatear el subtotal como moneda.
    	•	Se usó formatCurrency(subtotal) en la plantilla para mostrar el subtotal con dos decimales y el símbolo de dólar.
    
    Ahora, el subtotal del carrito se actualiza automáticamente en la vista sin necesidad de modificar manualmente la plantilla.

--- code/Luisdev/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ code/Luisdev/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -3,11 +3,15 @@ define(['uiComponent'], function (component) {
     console.log('Basic Free shipping component UI is loaded');
     return component.extend({
         defaults: {
+            subtotal : 30.00,
             template: 'Luisdev_FreeShippingPromo/free-shipping-banner'
         },
         initialize: function () {
             this._super();
             console.log(this.message);
+        },
+        formatCurrency : function (value){
+            return '$'+ value.toFixed(2);
         }
     });
 });
