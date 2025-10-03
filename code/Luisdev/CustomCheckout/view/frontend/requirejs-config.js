7ae8d5fbceb384f54c37e7bcdcdfd35dcc2aafba
PerezContrerasLuis <luisperezcontreras@gmail.com>
2025-10-02 19:49:50 -0600

    Método alternativo para sobrescribir plantillas Knockout.js
    Qué se hace:
    Se reemplaza la plantilla sidebar.html de Magento con una versión personalizada, pero en lugar de hacerlo con XML, se usa RequireJS para forzar el cambio.
    
    Por qué se hace:
    A veces la plantilla que queremos modificar está muy “enterrada” en el código o se necesita una solución rápida. El método con RequireJS es más sencillo de aplicar en estos casos, aunque menos flexible para que otros módulos lo extiendan.
    
    Cómo se hace:
    	1.	Se abre el archivo requirejs-config.js.
    	2.	Se crea un nuevo valor dentro del objeto map.
    	3.	Se define un asterisco * para que el cambio aplique en todos los módulos de RequireJS.
    	4.	Se asigna la ruta de la plantilla original de Magento (Magento_Checkout/template/sidebar.html).
    	5.	Se indica como valor la ruta de la nueva plantilla personalizada, por ejemplo:
    "Luisdev_Customcheckout/template/sidebar.html".
    
    Con esto, Magento cargará siempre la nueva plantilla en lugar de la original.
    
    En resumen, este método sirve para reemplazar plantillas Knockout rápidamente con RequireJS, aunque se recomienda usar XML cuando se busca mayor compatibilidad y extensibilidad.

--- code/Luisdev/CustomCheckout/view/frontend/requirejs-config.js
+++ code/Luisdev/CustomCheckout/view/frontend/requirejs-config.js
@@ -1,5 +1,11 @@
 let config = {
     deps: [
         'Luisdev_CustomCheckout/js/mask-telephone-inputs'
-    ]
+    ],
+    map: {
+        '*': {
+            'Magento_Checkout/template/sidebar.html':
+                'Luisdev_CustomCheckout/template/sidebar.html'
+        }
+    }
 };
